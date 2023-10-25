7398d9f241c1c6afc1a8eb736f362ed976c5b85a
Victor Assis <victorluisgassis@gmail.com>
2023-10-24 23:14:09 -0300

    Create utils.pp to preprocess all data

---
+++ utils.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+import scanpy as sc
+import seaborn as sns
+import scvi
+
+def pp(csv_path, ribo_genes):
+    adata = sc.read_csv(csv_path).T
+    sc.pp.filter_genes(adata, min_cells = 10)
+    sc.pp.highly_variable_genes(adata, n_top_genes = 2000, subset = True, flavor = 'seurat_v3')
+    scvi.model.SCVI.setup_anndata(adata)
+    vae = scvi.model.SCVI(adata)
+    vae.train()
+    solo = scvi.external.SOLO.from_scvi_model(vae)
+    solo.train()
+    df = solo.predict()
+    df['prediction'] = solo.predict(soft = False)
+    df.index = df.index.map(lambda x: x[:-2])
+    df['dif'] = df.doublet - df.singlet
+    doublets = df[(df.prediction == 'doublet') & (df.dif > 1)]
+    
+    adata = sc.read_csv(csv_path).T
+    adata.obs['Sample'] = csv_path.split('_')[2] #'raw_counts/GSM5226574_C51ctr_raw_counts.csv'
+    
+    adata.obs['doublet'] = adata.obs.index.isin(doublets.index)
+    adata = adata[~adata.obs.doublet]
+    
+    
+    sc.pp.filter_cells(adata, min_genes=200) #get rid of cells with fewer than 200 genes
+    #sc.pp.filter_genes(adata, min_cells=3) #get rid of genes that are found in fewer than 3 cells
+    adata.var['mt'] = adata.var_names.str.startswith('mt-')  # annotate the group of mitochondrial genes as 'mt'
+    adata.var['ribo'] = adata.var.index.isin(ribo_genes)
+    sc.pp.calculate_qc_metrics(adata, qc_vars=['mt', 'ribo'], percent_top=None, log1p=False, inplace=True)
+    upper_lim = np.quantile(adata.obs.n_genes_by_counts.values, .98)
+    adata = adata[adata.obs.n_genes_by_counts < upper_lim]
+    adata = adata[adata.obs.pct_counts_mt < 20]
+    adata = adata[adata.obs.pct_counts_ribo < 2]
+
+    return adata
